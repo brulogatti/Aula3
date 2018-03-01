304ef32c475d09eb38670b480317edcad09cd422
brulogatti <bru.logatti@gmail.com>
2018-03-01 20:39:31 -0300

    Aula2RMIComum1

---
+++ src/comum/IServicoRemoto.java
@@ -0,0 +1,11 @@
+package comum;
+
+import java.util.List;
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IServicoRemoto extends Remote {
+	void inserir (Pessoa p) throws RemoteException;
+	List<Pessoa> listarPessoa() throws RemoteException;
+
+}
